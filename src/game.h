1b8ba19054cf82224007552714c02feb09669ddc
Povedano <guipoved@gmail.com>
2025-02-10 10:27:07 +0100

    merged game_reader_AGL

--- src/game.h
+++ src/game.h
@@ -17,11 +17,6 @@
 
 #define MAX_SPACES 100
 
-/** 
- * @brief Game
- * 
- * This struct stores all the information related to a game.
-*/
 typedef struct _Game {
   Id player_location;
   Id object_location;
@@ -31,126 +26,32 @@ typedef struct _Game {
   Bool finished;
 } Game;
 
-/** 
- * @brief It initializes a given game to the default. 
- * @author Profesores PPROG
- * 
- * @param game a pointer to the game to be modified. 
- * @return OK, if everything goes well or ERROR if there was some mistake.
-*/
+Status game_add_space(Game *game, Space *space);
+
 Status game_create(Game *game);
 
-/** 
- * @brief It initializes a game from a given file whose name is passed by argument. 
- * @author Profesores PPROG
- * 
- * @param game a pointer to teh game to be intialized. 
- * @param filename meant to be a .dat file containing all of the spaces information - Ids, Names, and Connections. 
- * @return OK, if everything goes well or ERROR if there was some mistake.
-*/
 Status game_create_from_file(Game *game, char *filename);
 
-/** 
- * @brief Frees allocated memory for game->spaces and game->last_cmd. 
- * @author Profesores PPROG.
- * 
- * @param game Pointer to a game variable.
- * @return OK, if everything goes well or ERROR if there was some mistake.
-*/
 Status game_destroy(Game *game);
 
-/** 
- * @brief Given a pointer to a game and a space Id, returns a space struct or null. 
- * @author Profesores PPROG
- * 
- * @param game A pointer to game where the space exists. 
- * @param id Id of the desired space
- * @return The desired space if it can be found within the given game, or null if it cannot. 
-*/
 Space *game_get_space(Game *game, Id id);
 
-/** 
- * @brief It retrieves the current player location, stored in game struct
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @return Id of the space where the player is currently located. 
-*/
 Id game_get_player_location(Game *game);
 
-/** 
- * @brief Matches the current player location to a space Id passed by argument
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @param id The id of the space the player must be moved to
- * @return OK if everything went well, or ERROR if an error ocurred. 
-*/
 Status game_set_player_location(Game *game, Id id);
 
-/** 
- * @brief It retrieves the current object location, stored in game struct
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @return Id of the space where the object is currently located. 
-*/
 Id game_get_object_location(Game *game);
 
-/** 
- * @brief Matches the current object location to a space Id passed by argument
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @param id The id of the space the object must be moved to
- * @return OK if everything went well, or ERROR if an error ocurred. 
-*/
 Status game_set_object_location(Game *game, Id id);
 
-/** 
- * @brief Retrieves the last command input stored in the game struct
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @return Pointer to the last command 
-*/
 Command* game_get_last_command(Game *game);
 
-/** 
- * @brief Assigns game->last_cmd pointer to the command pointer passed by argument. 
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @param command A pointer to the last command input by the user 
- * @return OK if everything went well, and ERROR if an error ocurred 
-*/
 Status game_set_last_command(Game *game, Command *command);
 
-/** 
- * @brief Checks value of the boolean variable game->finished 
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @return TRUE if game->finished == TRUE or FALSE if game->finished == FALSE
-*/
 Bool game_get_finished(Game *game);
 
-/** 
- * @brief Updates the value of the game->finished boolean variable
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
- * @param finished Boolean variable to assign to game->finished
- * @return OK if everything went well or ERROR if an error ocurred
-*/
 Status game_set_finished(Game *game, Bool finished);
 
-/** 
- * @brief Prints the current game state
- * @author Profesores PPROG
- * 
- * @param game A pointer to the current game struct
-*/
 void game_print(Game *game);
 
 #endif
---
+++ src/game_reader.c
@@ -0,0 +1,76 @@
+/**
+ * @brief It currently implements the load of spaces (it may later also implement load of other elements of the game)
+ *
+ * @file game_reader.c
+ * @author  PPROG Group 2 (GPA, AGL)
+ * @version 0
+ * @date 05-02-2025
+ * @copyright GNU Public License
+ */
+
+
+#include "game.h"
+#include "space.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Game_reader implementation: load of spaces*/
+
+
+Status game_reader_load_spaces(Game *game, char *filename) {
+  FILE *file = NULL;
+  char line[WORD_SIZE] = "";
+  char name[WORD_SIZE] = "";
+  char *toks = NULL;
+  Id id = NO_ID, north = NO_ID, east = NO_ID, south = NO_ID, west = NO_ID;
+  Space *space = NULL;
+  Status status = OK;
+
+  if (!filename) {
+    return ERROR;
+  }
+
+  file = fopen(filename, "r");
+  if (file == NULL) {
+    return ERROR;
+  }
+
+  while (fgets(line, WORD_SIZE, file)) {
+    if (strncmp("#s:", line, 3) == 0) {
+      toks = strtok(line + 3, "|");
+      id = atol(toks);
+      toks = strtok(NULL, "|");
+      strcpy(name, toks);
+      toks = strtok(NULL, "|");
+      north = atol(toks);
+      toks = strtok(NULL, "|");
+      east = atol(toks);
+      toks = strtok(NULL, "|");
+      south = atol(toks);
+      toks = strtok(NULL, "|");
+      west = atol(toks);
+#ifdef DEBUG
+      printf("Leido: %ld|%s|%ld|%ld|%ld|%ld\n", id, name, north, east, south, west);
+#endif
+      space = space_create(id);
+      if (space != NULL) {
+        space_set_name(space, name);
+        space_set_north(space, north);
+        space_set_east(space, east);
+        space_set_south(space, south);
+        space_set_west(space, west);
+        game_add_space(game, space);
+      }
+    }
+  }
+
+  if (ferror(file)) {
+    status = ERROR;
+  }
+
+  fclose(file);
+
+  return status;
+}
