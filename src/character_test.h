6ab4332dafd1ecbe7079c1774eecb37fb382b6a1
povedan0 <guilherme.povedano@estudiante.uam.es>
2025-02-28 20:24:55 +0100

    Created entire character_test module

---
+++ src/character_test.h
@@ -0,0 +1,96 @@
+#ifndef CHARACTER_TEST_H
+#define CHARACTER_TEST_H
+
+#include "character.h"
+#include "test.h"
+#include "types.h"
+
+void test1_character_create();
+
+void test2_character_create();
+
+void test3_character_create();
+
+void test1_character_destroy();
+
+void test2_character_destroy();
+
+void test1_character_set_name();
+
+void test2_character_set_name();
+
+void test3_character_set_name();
+
+void test4_character_set_name();
+
+void test5_character_set_name();
+
+void test1_character_set_gdesc();
+
+void test2_character_set_gdesc();
+
+void test3_character_set_gdesc();
+
+void test4_character_set_gdesc();
+
+void test5_character_set_gdesc();
+
+void test1_character_increase_health();
+
+void test2_character_increase_health();
+
+void test3_character_increase_health();
+
+void test4_character_increase_health();
+
+void test5_character_increase_health();
+ 
+void test1_character_remove_health();
+
+void test2_character_remove_health();
+
+void test3_character_remove_health();
+
+void test4_character_remove_health();
+
+void test5_character_remove_health();
+
+void test1_character_set_friendly();
+
+void test2_character_set_friendly();
+
+void test1_character_set_message();
+
+void test2_character_set_message();
+
+void test3_character_set_message();
+
+void test4_character_set_message();
+
+void test5_character_set_message();
+
+void test1_character_get_id();
+
+void test2_character_get_id();
+
+void test1_character_get_name();
+
+void test2_character_get_name();
+
+void test1_character_get_gdesc();
+
+void test2_character_get_gdesc();
+
+void test1_character_get_health();
+
+void test2_character_get_health();
+
+void test1_character_get_friendly();
+
+void test2_character_get_friendly();
+
+void test1_character_get_message();
+
+void test2_character_get_message();
+
+#endif
