d889c8342fb905b6d25b9eb82555272d5a4af67a
povedan0 <guilherme.povedano@estudiante.uam.es>
2025-11-10 23:34:06 +0100

    mudanca pequena

--- src/types.h
+++ src/types.h
@@ -14,7 +14,7 @@
 #define WORD_SIZE 1000 /** constant for the size of variable names */
 #define NO_ID -1 /** constant for an invalid ID */
 
-typedef long Id; /** alias for long integer */
+typedef long Id; /** alias for long int */
 
 typedef enum { FALSE, TRUE } Bool; /** boolean variable declaration */
 
